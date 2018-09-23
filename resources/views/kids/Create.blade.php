77c53b0b7813153982bf7e4ecb9de478251ae948
Eliecer <narvaezeliecer@gmail.com>
2018-09-22 18:28:55 -0600

    Narvaez forever

---
+++ resources/views/kids/Create.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="row">
+  <div class="col-md-12 col-sm-12 col-xs-12">
+    <div class="x_panel">
+      <div class="x_title">
+        <h2>Form Design <small>different form elements</small></h2>
+        <ul class="nav navbar-right panel_toolbox">
+          <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+          </li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+            <ul class="dropdown-menu" role="menu">
+
+            </ul>
+          </li>
+          <li><a class="close-link"><i class="fa fa-close"></i></a>
+          </li>
+        </ul>
+        <div class="clearfix"></div>
+      </div>
+      <div class="x_content">
+        <br />
+        <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
+
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nombre completo <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="name" name="name" required="required" class="form-control col-md-7 col-xs-12">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Cédula <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input type="text" id="identification" name="identification" required="required" class="form-control col-md-7 col-xs-12">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Edad</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <input id="age" class="form-control col-md-7 col-xs-12" type="number" name="age">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12">Gender</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <div id="gender" class="btn-group" data-toggle="buttons">
+                <label class="btn btn-default" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                  <input type="radio" name="gender" value="male"> &nbsp; Masculino &nbsp;
+                </label>
+                <label class="btn btn-primary" data-toggle-class="btn-primary" data-toggle-passive-class="btn-default">
+                  <input type="radio" name="gender" value="female"> Femenino
+                </label>
+              </div>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Etnia</label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <select name="etnia" class="form-control col-md-7 col-xs-12"  required>
+                <option value="">Afro costarricense</option>
+                <option value="press">Asiático</option>
+                <option value="net">Blanco</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Tratamientos de índole física </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <textarea id="message" required="required" class="form-control" name="message" data-parsley-trigger="keyup" data-parsley-minlength="20" data-parsley-maxlength="100" data-parsley-minlength-message="Come on! You need to enter at least a 20 caracters long comment.."
+                data-parsley-validation-threshold="10"></textarea>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Cuadros de enfermedades presentados en los últimos 6 meses </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+              <textarea id="message" required="required" class="form-control" name="message" data-parsley-trigger="keyup" data-parsley-minlength="20" data-parsley-maxlength="100" data-parsley-minlength-message="Come on! You need to enter at least a 20 caracters long comment.."
+                data-parsley-validation-threshold="10"></textarea>
+            </div>
+          </div>
+
+          <div class="ln_solid"></div>
+          <div class="form-group">
+            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+              <button class="btn btn-primary" type="button">Cancelar</button>
+              <button class="btn btn-primary" type="reset">Borrar</button>
+              <button type="submit" class="btn btn-success">Enviar</button>
+            </div>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
